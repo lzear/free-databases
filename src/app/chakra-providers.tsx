15bb254feafbe84376f74bf87b9344ccfa8bcc92
lzear <antoine.clausse@gmail.com>
2023-05-13 08:29:31 +0200

    Add chakra-ui https://chakra-ui.com/getting-started/nextjs-guide

---
+++ src/app/chakra-providers.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import React from 'react'
+import { CacheProvider } from '@chakra-ui/next-js'
+import { ChakraProvider } from '@chakra-ui/react'
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <CacheProvider>
+      <ChakraProvider>{children}</ChakraProvider>
+    </CacheProvider>
+  )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import { Providers } from './chakra-providers'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,7 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   )
 }
