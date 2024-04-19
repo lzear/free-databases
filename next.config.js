87ba442ba482d1be961119e018f75292d81a43e0
lzear <antoine.clausse@gmail.com>
2024-04-19 23:22:12 +0200

    Run `yarn ncu && yarn`

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverActions: true,
-  },
+  experimental: {},
 }
 
-// eslint-disable-next-line unicorn/prefer-module
 export default nextConfig
