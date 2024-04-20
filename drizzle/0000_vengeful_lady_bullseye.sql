49ebb88fed8513ed8d2d6f0a5961cfbd71b7302c
lzear <antoine.clausse@gmail.com>
2024-04-20 02:38:36 +0200

    Migrate for Postgres

--- drizzle/0000_vengeful_lady_bullseye.sql
+++ drizzle/0000_vengeful_lady_bullseye.sql
@@ -1,6 +1,7 @@
-CREATE TABLE `todos` (
-	`id` varchar(255) PRIMARY KEY NOT NULL,
-	`created_at` timestamp NOT NULL DEFAULT (now()),
-	`updated_at` timestamp NOT NULL DEFAULT (now()),
-	`name` text NOT NULL,
-	`done` boolean NOT NULL);
+CREATE TABLE "todos" (
+    "id" varchar(255) PRIMARY KEY NOT NULL,
+    "created_at" timestamp NOT NULL DEFAULT now(),
+    "updated_at" timestamp NOT NULL DEFAULT now(),
+    "name" text NOT NULL,
+    "done" boolean NOT NULL
+);
