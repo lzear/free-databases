dbfff0dc477f17f406698193ed57e5a9a5eba68b
lzear <antoine.clausse@gmail.com>
2024-04-21 20:57:42 +0200

    Load list using "Suspense"

--- src/todos/todos.tsx
+++ src/todos/todos.tsx
@@ -1,11 +1,12 @@
 import 'server-only'
 
-import React from 'react'
+import React, { Suspense } from 'react'
 
-import { CardGrid } from '../components/card'
-import { DatabaseSlug } from '../databases'
-import { todoProviders } from '../todo-providers'
-import { todoToDto } from '../todos-server/type'
+import { CardLoading } from '@/components/card'
+import { DatabaseSlug } from '@/databases'
+import { todoProviders } from '@/todo-providers'
+import { todoToDto } from '@/todos-server/type'
+
 import { TodoComponent } from './todo-component'
 
 type Props = {
@@ -15,27 +16,48 @@ type Props = {
   prepend?: React.ReactNode
 }
 
-export const TodoList = async ({ done, provider, title, prepend }: Props) => {
+const TodosAsync = async ({
+  done,
+  provider,
+}: {
+  done: boolean
+  provider: DatabaseSlug
+}) => {
+  const todoProvider = todoProviders[provider]
+  const todos = await todoProvider.server?.getTodos(done)
+
+  if (!todos || todos.length === 0)
+    return (
+      <div className="min-h-12 px-2 py-4 opacity-60">
+        <p>
+          <em>No items in the list...</em>
+        </p>
+      </div>
+    )
+
+  return todos?.map((todo) => (
+    <TodoComponent key={todo.id} todo={todoToDto(todo)} provider={provider} />
+  ))
+}
+
+export const LoadingTodos = () =>
+  Array.from({ length: Math.floor(Math.random() * 3) + 1 }, (_, i) => (
+    <CardLoading key={i} />
+  ))
+
+export const TodoList = ({ done, provider, title, prepend }: Props) => {
   const todoProvider = todoProviders[provider]
   if (!todoProvider) throw new Error(`Unknown provider ${provider}`)
 
-  const todos = await todoProvider.server?.getTodos(done)
-
   return (
-    <>
-      {(prepend || (todos && todos.length > 0)) && (
-        <h2 style={{ margin: '40px 0 20px' }}>{title}</h2>
-      )}
-      <CardGrid>
+    <div className="mt-16 flex flex-col gap-5">
+      <h2>{title}</h2>
+      <div className="grid grid-cols-1 gap-1 sm:grid-cols-2 lg:grid-cols-4">
         {prepend}
-        {todos?.map((todo) => (
-          <TodoComponent
-            key={todo.id}
-            todo={todoToDto(todo)}
-            provider={provider}
-          />
-        ))}
-      </CardGrid>
-    </>
+        <Suspense fallback={<LoadingTodos />}>
+          <TodosAsync done={done} provider={provider} />
+        </Suspense>
+      </div>
+    </div>
   )
 }
